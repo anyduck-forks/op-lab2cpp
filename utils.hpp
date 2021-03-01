2b63321872bbefe5ae56d489f6115a0c26fa498d
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-03-01 14:00:05 +0200

    Add: comments to functions in utils.hhp, student.hpp

--- utils.hpp
+++ utils.hpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <vector>
 
+// конвертує рядок в булеву змінну
 bool flag2bool(std::string);
+
+// поділяє рядок за розділювачем
 std::vector<std::string> split(std::string, char);
