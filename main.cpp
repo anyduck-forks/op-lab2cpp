a9d7569145a2398df24cf986818107f1a70bbd10
Roman <roman_belowski@protonmail.com>
2021-03-08 12:52:45 +0200

    Add: Prototypes and minimal mark printing

--- main.cpp
+++ main.cpp
@@ -8,48 +8,10 @@
 
 using namespace std;
 
-string getDirectory()
-{
-    string directory;
-    cout << "Введіть шлях папки з CSV фалами: ";
-    getline(cin, directory);
-
-    return directory;
-}
-
-// Зчитує дані з csv файлу у вектор з studentIn
-vector<studentIn> loadCsv(string path)
-{
-    vector<studentIn> out;
-    ifstream csv(path);
-    string line;
-
-    if (!csv.is_open())
-    {
-        cerr << "Не вдалося відкрити файл - \"" + path + "\"" << endl;
-        return out;
-    }
-
-    getline(csv, line); // пропускаємо перший рядок
-    while (getline(csv, line))
-        out.push_back(csv2studentIn(line));
-    csv.close();
-    return out;
-}
+string getDirectory();
 
 // Зчитує дані з усіх csv файлів в папці у вектор з studentIn
-vector<studentIn> loadTable(string direcotry)
-{
-    vector<studentIn> out, tmp;
-    vector<string> files = listFiles(direcotry, ".csv");
-    for (string file : files)
-    {
-        tmp = loadCsv(file);
-        out.insert(out.end(), tmp.begin(), tmp.end());
-    }
-
-    return out;
-}
+vector<studentIn> loadTable(string direcotry);
 
 int main()
 {
@@ -75,5 +37,48 @@ int main()
     cout << "===========" << endl;
     for (studentOut rating_student : table_rating)
         print(rating_student);
+    cout << "Мінімальна оцінка для стипендії це " << table_rating.back().avgRating << endl;
     return 0;
+}
+
+
+string getDirectory()
+{
+    string directory;
+    cout << "Введіть шлях папки з CSV фалами: ";
+    getline(cin, directory);
+
+    return directory;
+}
+
+vector<studentIn> loadCsv(string path)
+{
+    vector<studentIn> out;
+    ifstream csv(path);
+    string line;
+
+    if (!csv.is_open())
+    {
+        cerr << "Не вдалося відкрити файл - \"" + path + "\"" << endl;
+        return out;
+    }
+
+    getline(csv, line); // пропускаємо перший рядок
+    while (getline(csv, line))
+        out.push_back(csv2studentIn(line));
+    csv.close();
+    return out;
+}
+
+vector<studentIn> loadTable(string direcotry)
+{
+    vector<studentIn> out, tmp;
+    vector<string> files = listFiles(direcotry, ".csv");
+    for (string file : files)
+    {
+        tmp = loadCsv(file);
+        out.insert(out.end(), tmp.begin(), tmp.end());
+    }
+
+    return out;
 }
